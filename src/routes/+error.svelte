657ba3699ee9dc5c295f97e1d92f726f3726e14f
Adam <rumpelteam@gmail.com>
2022-11-11 18:12:25 +0100

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from '$app/stores';
+	import image from '$lib/assets/404.svg';
+</script>
+
+<img src={image} alt="" />
+<h1>Błąd {$page.status}</h1>
+<p>{$page.error.message}</p>
+
+<style>
+	img {
+		max-width: 50%;
+		display: block;
+		margin: 5rem auto;
+	}
+
+	h1,
+	p {
+		text-align: center;
+	}
+</style>
